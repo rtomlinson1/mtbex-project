fff0bba5f25561c099e9500744c806ee469f123e
Ryan Tomlinson <rltomlinson@outlook.com>
2018-09-10 15:36:49 -0500

    Initial commit for new mtb explained website

---
+++ mtbex/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mtbex project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mtbex.settings')
+
+application = get_wsgi_application()
---
+++ vids/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VidsConfig(AppConfig):
+    name = 'vids'
